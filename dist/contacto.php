4f76b6d200a4a588b02a7af8f903b782fc4e6da4
oskamaya <oskamaya@gmail.com>
2014-10-08 23:35:02 -0500

    validation works!

--- dist/contacto.php
+++ dist/contacto.php
@@ -4,34 +4,34 @@ $hasError = false;
 $sent = false;
 
 if(isset($_POST['submitform'])) {
-  $name = trim(htmlspecialchars($_POST['name'], ENT_QUOTES));
-  $lastname = trim(htmlspecialchars($_POST['lastname'], ENT_QUOTES));
-  $email = trim($_POST['email']);
-  $message = trim(htmlspecialchars($_POST['message'], ENT_QUOTES));
+  $name = trim(htmlspecialchars($_POST['fname'], ENT_QUOTES));
+  $lastname = trim(htmlspecialchars($_POST['flastname'], ENT_QUOTES));
+  $email = trim($_POST['femail']);
+  $message = trim(htmlspecialchars($_POST['fmessage'], ENT_QUOTES));
 
   $fieldsArray = array(
-    'name' => $name,
-    'lastname' => $lastname,
-    'email' => $email,
-    'message' => $message
+    'fname' => $name,
+    'flastname' => $lastname,
+    'femail' => $email,
+    'fmessage' => $message
   );
 
   $errorArray = array();
 
   foreach ($fieldsArray as $key => $val) {
     switch ($key) {
-      case 'name':
-      case 'lastname':
-      case 'message':
+      case 'fname':
+      case 'flastname':
+      case 'fmessage':
         if(empty($val)) {
           $hasError = true;
-          $errorArray[$key] = ucfirst($key) . "El campo ha quedado vacio";
+          $errorArray[$key] = ucfirst($key) . "El campo ha quedado vacío";
         }
         break;
-      case 'email':
+      case 'femail':
         if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
           $hasError = true;
-          $errorArray[$key] = "Email invalido";
+          $errorArray[$key] = "Email inválido";
         } else {
           $email = filter_var($email, FILTER_SANITIZE_EMAIL);
         }
@@ -40,12 +40,11 @@ if(isset($_POST['submitform'])) {
   }
 
   if($hasError !== true) {
-    $to = "oskamaya@gmail.com";
-    $subject = "Contacto - PYP Proyectos";
-    $msgcontents = "Nombre: $name<br>Apellidos: $lastname<br>Email: $email<br> Mensaje: $message";
-    $headers = "MIME-Version 1.0 \r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1 \r\n";
-    $headers .= "From: $name <$email> \r\n";
+    $to           = "oskamaya@yahoo.com.co";
+    $subject      = "Contacto - PYP Proyectos";
+    $msgcontents  = "Nombre: $name<br>Apellidos: $lastname<br>Email: $email<br> Mensaje: $message";
+    $headers      = 'From: $name <$email>' . "\r\n" .
+                    'X-Mailer: PHP/' . phpversion();
     $mailsent = mail($to, $subject, $msgcontents, $headers);
     if ($mailsent) {
       $sent = true;
@@ -61,30 +60,30 @@ if(isset($_POST['submitform'])) {
 
 <!DOCTYPE html>
 <html lang="en">
-  	<head>
-	   <meta charset="utf-8">
-	   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-	   <meta name="viewport" content="width=device-width, initial-scale=1">
-	   <title>P&P Proyectos - Proyectos con Sentido</title>
+    <head>
+     <meta charset="utf-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>P&P Proyectos - Proyectos con Sentido</title>
 
-	   <!-- Bootstrap -->
-	   <link href="css/bootstrap.css" rel="stylesheet">
-	   <link href="css/styles.css" rel="stylesheet">
+     <!-- Bootstrap -->
+     <link href="css/bootstrap.css" rel="stylesheet">
+     <link href="css/styles.css" rel="stylesheet">
 
-	   <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-	   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	   <!--[if lt IE 9]>
-	      <script src="js/html5shiv.js"></script>
-	      <script src="js/respond.min.js"></script>
-    	<![endif]-->
-  	</head>
-  	<body>
+     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+     <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+        <script src="js/respond.min.js"></script>
+      <![endif]-->
+    </head>
+    <body>
    <!-- <div class="container"> starts container-->
-    	<nav class="navbar navbar-default" role="navigation">
-     		<div class="container">
-        		<div class="container-fluid">
-         	<!-- Brand and toggle get grouped for better mobile display -->
-          	<div class="navbar-header">
+      <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <div class="container-fluid">
+          <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
@@ -134,7 +133,7 @@ if(isset($_POST['submitform'])) {
 
             <!-- formulario -->
             <form id="contactForm" method="post" class="form-horizontal" action="<?php echo $_SERVER['PHP_SELF']; ?>" novalidate>
-              	<?php  
+                <?php  
                   if($sent === true) {
                     echo "<h2 class='success'> Gracias, su mensaje ha sido enviado satisfactoriamente </h2>";
                   } elseif($hasError === true) {
@@ -147,34 +146,34 @@ if(isset($_POST['submitform'])) {
                 ?>
 
                 <div class="form-group">
-                	<label for="inputName" class="col-sm-3 control-label">Nombres</label>
-                	<div class="col-sm-9">
-                  	<input id="fname" type="text" class="form-control" name="name" value="<?php echo(isset($name) ? $name : ""); ?>">
-               	</div>
-             	</div>
-              	<div class="form-group">
-               	<label for="inputLastname" class="col-sm-3 control-label">Apellidos</label>
-                	<div class="col-sm-9">
-                  	<input id="flastname" type="text" class="form-control" name="lastname" value="<?php echo(isset($lastname) ? $lastname : ""); ?>">
-                	</div>
-              	</div>
-              	<div class="form-group">
-               	<label for="inputEmail" class="col-sm-3 control-label">Email</label>
-                	<div class="col-sm-9">
-                  <input id="femail" type="email" class="form-control" name="email" value="<?php echo(isset($email) ? $email : ""); ?>">
+                  <label for="inputName" class="col-sm-3 control-label">Nombres</label>
+                  <div class="col-sm-9">
+                    <input id="name" type="text" class="form-control" name="fname" value="<?php echo(isset($name) ? $name : ""); ?>">
+                </div>
+              </div>
+                <div class="form-group">
+                <label for="inputLastname" class="col-sm-3 control-label">Apellidos</label>
+                  <div class="col-sm-9">
+                    <input id="lastname" type="text" class="form-control" name="flastname" value="<?php echo(isset($lastname) ? $lastname : ""); ?>">
+                  </div>
+                </div>
+                <div class="form-group">
+                <label for="inputEmail" class="col-sm-3 control-label">Email</label>
+                  <div class="col-sm-9">
+                  <input id="email" type="email" class="form-control" name="femail" value="<?php echo(isset($email) ? $email : ""); ?>">
                </div>
-              	</div>
-              	<div class="form-group">
-               	<label class="col-sm-3 control-label">Su mensaje</label>
-                	<div class="col-sm-9">
-                  	<textarea id="fmessage" class="form-control" name="message" rows="3" value="<?php echo(isset($message) ? $message : ""); ?>"></textarea>
-                	</div>
-              	</div>
-              	<div class="form-group">
-                	<div class="col-sm-offset-3 col-sm-9">
-                  	<button type="submit" name="submitform" class="btn btn-primary">ENVIAR DATOS</button>
-                	</div>
-              	</div>
+                </div>
+                <div class="form-group">
+                <label class="col-sm-3 control-label">Su mensaje</label>
+                  <div class="col-sm-9">
+                    <textarea id="message" class="form-control" name="fmessage" rows="3" value="<?php echo(isset($message) ? $message : ""); ?>"></textarea>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="col-sm-offset-3 col-sm-9">
+                    <input type="submit" name="submitform" class="btn btn-primary" value="ENVIAR DATOS">
+                  </div>
+                </div>
             </form>
             <!-- termina el formulario -->
 
@@ -226,87 +225,52 @@ if(isset($_POST['submitform'])) {
       </div>
     </footer>
     
-   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-   <!-- Include all compiled plugins (below), or include individual files as needed -->
-   <script src="js/bootstrap.min.js"></script>
-   <!-- jquery validation link -->
-   
-   <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
-   <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/additional-methods.js"></script>
-   <script type="text/javascript">
-   jQuery(document).ready(function($) {
-      $("#contactForm").validate({
-        debug: true
-        	// rules: {
-        	// 	name: {
-        	// 		required: true,
-        	// 		minlenght: 2
-        	// 	},
-        	// 	lastname: {
-        	// 		required: true,
-        	// 		minlenght: 2
-        	// 	},
-        	// 	email: {
-        	// 		required: true,
-        	// 		email: true
-        	// 	},
-        	// 	message: {
-        	// 		required: true,
-        	// 		minlenght: 10
-        	// 	}
-        	// },
-        	// messages: {
-        	// 	name: {
-         //      required: "Este campo es obligatorio",
-        	// 		minlenght: "Un nombre no puede tener una sola letra"
-        	// 	},
-        	// 	lastname: {
-        	// 		required: "Este campo es obligatorio",
-        	// 		minlenght: "Un apellido no puede tener una sola letra"
-        	// 	},
-        	// 	email: {
-        	// 		required: "Ingrese tu dirección de correo",
-        	// 		email: "Ingresa una dirección de correo válida"
-        	// 	},
-        	// 	message: {
-        	// 		required: "Ingrese su mensaje",  
-        	// 		minlenght: "Su mensaje no puede ser tan corto"
-        	// 	}
-        	// }
-     	});
-    $("#fname").rules("add", {
-      required: true,
-      minlenght: 2,
-      message: {
-        required: "Este campo es obligatorio",
-        minlenght: "Ingrese un nombre válido"
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="js/bootstrap.min.js"></script>
+  <!-- jquery validation link -->
+  <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.13.0/jquery.validate.min.js"></script>
+  <script type="text/javascript">
+  jQuery(document).ready(function($) {
+    $("#contactForm").validate({
+      rules: {
+        fname: {
+          required: true,
+          minlength: 2
+        },
+        flastname: {
+          required: true,
+          minlength: 2
+        },
+        femail: {
+          required: true,
+          email: true
+        },
+        fmessage: {
+          required: true,
+          minlength: 10
+        }
+      },
+      messages: {
+        fname: {
+          required: "Este campo es obligatorio",
+          minlength: "Un nombre no puede tener una sola letra"
+        },
+        flastname: {
+          required: "Este campo es obligatorio",
+          minlength: "Un apellido no puede tener una sola letra"
+        },
+        femail: {
+          required: "Ingrese tu dirección de correo",
+          email: "Ingresa una dirección de correo válida"
+        },
+        fmessage: {
+          required: "Ingrese su mensaje", 
+          minlength: "Su mensaje no puede ser tan corto"
+        }
       }
     });
-    // $("#flastname").rules("add", {
-    //   required: true,
-    //   minlenght: 2,
-    //   messages: {
-    //     required: "este campo no puede quedar vacío",
-    //     minlenght: "Ingrese un nombre válido"
-    //   }
-    // });
-    // $("#femail").rules("add", {
-    //   required: true,
-    //   email: true,
-    //   messages: {
-    //     required: "este campo no puede quedar vacío",
-    //     email: "Ingrese un email válido"
-    //   }
-    // });
-    // $("#fmessage").rules("add", {
-    //   required: true,
-    //   minlenght: 10,
-    //   messages: {
-    //     required: "este campo no puede quedar vacío",
-    //     minlenght: "Tu mensaje no puede ser tan corto"
-    //   }
-    // });
    });
   </script>
   </body>
